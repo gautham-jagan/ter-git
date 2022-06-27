9e618f89723841144e02376a9fe3c5e3c8a3fce0
gautham-jagan <60955592+gautham-jagan@users.noreply.github.com>
2022-06-27 18:24:29 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -7,6 +7,6 @@ terraform {
   backend "gcs" {
     bucket = "myvik-tf-state-prod"
     prefix = "terraform/state"
-    credentials= "${var.crendentials}"
+    credentials= "${var.credentials}"
   }
 }
